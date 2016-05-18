8aa4b32cac0daf253f93f74def98ee079ac68f29
#云淡然 <cloudcome@163.com>
2016-05-18 13:51:32 +0800

    blear.utils.date

--- test/test.index.js
+++ test/test.index.js
@@ -7,35 +7,79 @@
 
 'use strict';
 
-var querystring = require('../src/index.js');
+var date = require('../src/index.js');
 
 describe('index.js', function () {
-    it('.parse', function (done) {
-        expect(querystring.parse('a=1&c=5&b=2&c=3&d=4&c=6')).toEqual({
-            a: '1',
-            b: '2',
-            c: ['5', '3', '6'],
-            d: '4'
-        });
-
-        done();
+    it('.now', function () {
+        expect(typeof date.now()).toEqual('number');
     });
 
-    it('.stringify', function (done) {
-        var ret = querystring.stringify({
-            a: '1',
-            b: '2',
-            c: ['5', '3', '6'],
-            d: '4',
-            e: function(){}
-        });
+    it('.parse', function () {
+        expect(isNaN(date.parse('...'))).toEqual(false);
+        expect(isNaN(date.parse(new Date('...')))).toEqual(false);
+    });
 
-        expect(ret).toMatch(/a=1/);
-        expect(ret).toMatch(/b=2/);
-        expect(ret).toMatch(/c=5&c=3&c=6/);
-        expect(ret).toMatch(/d=4/);
-        expect(ret).not.toMatch(/e=/);
+    it('.format', function () {
+        expect(date.format()).toMatch(/^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}$/);
+        expect(date.format('YYYY'), new Date()).toMatch(/^\d{4}$/);
+    });
 
-        done();
+    it('.isLeapYear', function () {
+        expect(date.isLeapYear(2016)).toEqual(true);
+        expect(date.isLeapYear(2017)).toEqual(false);
+    });
+
+    it('.getDaysInMonth', function () {
+        expect(date.getDaysInMonth(2016, 1)).toEqual(29);
+        expect(date.getDaysInMonth(2017, 1)).toEqual(28);
+    });
+
+    it('.getDaysInYear', function () {
+        expect(date.getDaysInYear(2016, 0, 1)).toEqual(1);
+        expect(date.getDaysInYear(2016, 11, 31)).toEqual(366);
+    });
+
+    it('.getWeeksInYear', function () {
+        expect(date.getWeeksInYear(2016, 0, 1)).toEqual(1);
+        expect(date.getWeeksInYear(2016, 3, 11)).toEqual(16);
+        expect(date.getWeeksInYear(2016, 0, 1, 3)).toEqual(1);
+        expect(date.getWeeksInYear(2016, 3, 11, 3)).toEqual(15);
+    });
+
+    it('.getWeeksInMonth', function () {
+        expect(date.getWeeksInMonth(2016, 0, 1)).toEqual(1);
+        expect(date.getWeeksInMonth(2016, 3, 11)).toEqual(3);
+        expect(date.getWeeksInMonth(2016, 0, 1, 3)).toEqual(1);
+        expect(date.getWeeksInMonth(2016, 3, 11, 3)).toEqual(2);
+    });
+
+    it('.from', function () {
+        var now = date.now();
+
+        // 过去
+        expect(date.from(0, now)).toEqual('很久之前');
+        expect(date.from(now - 1, now)).toEqual('刚刚');
+        expect(date.from(now - 10 * 1000, now)).toEqual('10秒前');
+        expect(date.from(now - 30 * 60 * 1000, now)).toEqual('30分钟前');
+        expect(date.from(now - 60 * 60 * 1000, now)).toEqual('1小时前');
+        expect(date.from(now - 24 * 60 * 60 * 1000, now)).toEqual('1天前');
+        expect(date.from(now - 30 * 24 * 60 * 60 * 1000, now)).toEqual('1个月前');
+        expect(date.from(now - 12 * 30 * 24 * 60 * 60 * 1000, now)).toEqual('1年前');
+        expect(date.from(now - 9 * 12 * 30 * 24 * 60 * 60 * 1000, now)).toEqual('9年前');
+        expect(date.from(now - 10 * 12 * 30 * 24 * 60 * 60 * 1000, now)).toEqual('很久之前');
+
+        // 将来
+        expect(date.from(now + 1, now)).toEqual('即将');
+        expect(date.from(now + 10 * 1000, now)).toEqual('10秒后');
+        expect(date.from(now + 60 * 60 * 1000, now)).toEqual('1小时后');
+        expect(date.from(now + 24 * 60 * 60 * 1000, now)).toEqual('1天后');
+        expect(date.from(now + 30 * 24 * 60 * 60 * 1000, now)).toEqual('1个月后');
+        expect(date.from(now + 12 * 30 * 24 * 60 * 60 * 1000, now)).toEqual('1年后');
+        expect(date.from(now + 9 * 12 * 30 * 24 * 60 * 60 * 1000 + 1, now)).toEqual('9年后');
+        expect(date.from(now + 10 * 12 * 30 * 24 * 60 * 60 * 1000 + 1, now)).toEqual('很久之后');
+    });
+
+    it('.iso', function () {
+        expect(date.iso()).toMatch(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/);
     });
 });
